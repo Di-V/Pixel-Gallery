cc49114d8a1e9300ce94559b18bacb0a85c34faf
di-v <vaganov.dimitry@gmail.com>
2020-03-12 12:00:05 +0300

    initial commit

---
+++ app/src/main/java/app/di_v/pixel_gallery/MainActivity.kt
@@ -0,0 +1,19 @@
+package app.di_v.pixel_gallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.di_v.pixel_gallery.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/app/di_v/pixel_gallery/ui/main/MainViewModel.kt
@@ -0,0 +1,5 @@
+package app.di_v.pixel_gallery.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel()
